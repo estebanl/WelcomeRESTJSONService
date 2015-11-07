3f8c49919259671263ad047c08ea65a14b0ea1a5
Esteban Lopez <estebanlopez.co>
2015-11-07 15:01:55 -0500

    Servicio REST respuesta JSON

---
+++ WelcomeRESTJSONService/IWelcomeRESTJSONService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WelcomeRESTJSONService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IWelcomeRESTJSONService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWelcomeRESTJSONService
+    {
+
+        [OperationContract]
+        [WebGet (ResponseFormat = WebMessageFormat.Json, UriTemplate = "/welcome/{yourName}")]
+        TextMessage Welcome(string yourName);
+    }
+
+    [DataContract]
+    public class TextMessage
+    {
+        [DataMember]
+        public string Message { get; set; }
+    }
+}
---
+++ WelcomeRESTJSONService/WelcomeRESTJSONService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WelcomeRESTJSONService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "WelcomeRESTJSONService" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione WelcomeRESTJSONService.svc o WelcomeRESTJSONService.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WelcomeRESTJSONService : IWelcomeRESTJSONService
+    {
+        public TextMessage Welcome(string yourName)
+        {
+            TextMessage message = new TextMessage();
+            message.Message = string.Format("WelcomeServices With REST and JSON,{0}!", yourName);
+            return message;
+        }
+    }
+}
